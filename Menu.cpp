c2b118aa632def26be840f1ae344e98d3114af04
kellyolivier <36606873+kellyolivier@users.noreply.github.com>
2018-05-11 13:09:20 -0700

    Add files via upload
    kelly - version as of class on may 11, 2018

--- Menu.cpp
+++ Menu.cpp
@@ -7,3 +7,42 @@
 //
 
 #include "Menu.hpp"
+#include <iostream>
+
+using namespace std;
+
+void endPrompt()
+{
+    bool done = false;
+    char boolAns = ' ';
+    string name = "";
+    cout << " Would you like to review patient info? Enter y for yes and any other character for no" << endl;
+    cin >> boolAns;
+    boolAns = tolower(boolAns);
+    if (boolAns != 'y')
+        done = true;
+
+    while(!done)
+    {
+        cout << "Please enter the last name of the patient you wish to view the records of." << endl;
+        cin >> name;
+        for(int i =0; i<name.length(); i++)
+        {
+            name[i] = toupper(name[i]);
+        }
+        Patient record = Patient getRecord(std::string name);
+        //series of print statements for aspects of patient record
+    }
+        cout << "Thank you, come again!" << endl;
+}
+
+Patient getRecord(std::string name)
+{
+    Patient record;
+    
+    //search vector for name
+        //if no name found, turn null into error
+        //if name found, return object
+    
+    return record;
+}
--- Menu.hpp
+++ Menu.hpp
@@ -10,5 +10,12 @@
 #define Menu_hpp
 
 #include <stdio.h>
+#include <string>
+
+
+void endPrompt(); //prompts for the name and calls getRecord, then prints out patient info
+
+Patient getRecord(std::string name); //gets patient object from names vector and returns the object
+
 
 #endif /* Menu_hpp */
